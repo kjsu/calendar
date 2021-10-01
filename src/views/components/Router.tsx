8441a9e9a6a761c508d04b745fe4f3b3e30f95f4
kjsu <surtne@gmail.com>
2021-10-01 18:00:37 +0900

    add router path material ui dashboard

--- src/views/components/Router.tsx
+++ src/views/components/Router.tsx
@@ -2,15 +2,18 @@ import React, { Suspense } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from '~/views/pages/Home'
 import Main from '~/views/pages/Main'
+import Dashboard from '~/views/pages/Dashboard'
 
 export const MAIN = '/'
 export const HOME = '/home'
+export const MUI_DASHBOARD = '/dashboard'
 
 const Router: React.FC = () => (
   <Suspense fallback={<></>}>
     <Switch>
       <Route exact path={MAIN} component={Main} />
       <Route exact path={HOME} component={Home} />
+      <Route exact path={MUI_DASHBOARD} component={Dashboard} />
     </Switch>
   </Suspense>
 )
---
+++ src/views/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashboardContent from '~/views/components/Dashboard'
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <DashboardContent></DashboardContent>
+    </>
+  )
+}
+
+export default Dashboard
