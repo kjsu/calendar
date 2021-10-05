f6e22d6b61c7e4625807bc74aa6a85528657ed39
kjsu <surtne@gmail.com>
2021-10-05 19:26:33 +0900

    mui button에 router 연결

--- src/views/components/listCalendarItems.tsx
+++ src/views/components/listCalendarItems.tsx
@@ -4,16 +4,17 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PeopleIcon from '@mui/icons-material/People';
+import { Link } from 'react-router-dom';
 
 export const calendarItems = (
 	<div>
-		<ListItem button>
+		<ListItem button component={Link} to="/company">
 			<ListItemIcon>
 				<DashboardIcon />
 			</ListItemIcon>
-			<ListItemText primary="시간표" />
+			<ListItemText primary="회사" />
 		</ListItem>
-		<ListItem button>
+		<ListItem button component={Link} to="/personal">
 			<ListItemIcon>
 				<PeopleIcon />
 			</ListItemIcon>
--- src/views/pages/Home.tsx
+++ src/views/pages/Home.tsx
@@ -3,14 +3,20 @@ import { useRecoilState } from 'recoil'
 import calendarAction from '~/actions/calendarAction'
 import Calendar from '~/views/components/Calendar'
 import { schedulesAtom } from '~/recoil/calendarAtom'
+import { useParams } from 'react-router-dom'
+
+type HomeParams = {
+  type: string;
+};
 
 const Home: React.FC = () => {
   const [schedules, setSchedules] = useRecoilState(schedulesAtom)
+  const { type } = useParams<HomeParams>()
 
   useEffect(() => {
-    const schedules = calendarAction.getSchedules({ type: 'PERSONAL' })
+    const schedules = calendarAction.getSchedules({ type: type?.toUpperCase() })
     setSchedules(schedules)
-  }, [])
+  }, [type])
 
   return (
     <>
